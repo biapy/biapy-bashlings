85a5a4cda302d928b5ac61157f90ba1b9c613c72
Pierre-Yves Landuré <pierre-yves@landure.fr>
2022-10-30 19:19:00 +0100

    Cleanup documentation from deprecated alias function.

--- doc/realpath_check.md
+++ doc/realpath_check.md
@@ -16,32 +16,7 @@ Resolve a real absolute path and check its existance.
 If the file does not exists, display an error message and return error.
 Print its absolute real path on stdout if found.
 
-#### Example
-
-```bash
-source "${BASH_SOURCE[0]%/*}/libs/biapy-bashlings/src/realpath_check.bash"
-file_path="../relative/path"
-if file_realpath="$( realpath_check "${file_path}" )"; then
-  echo "File found. processing..."
-else
-  exit 1
-fi
-```
-
-#### Arguments
-
-* -q | --quiet Disable the error message.
-* -e | --exit Enable exiting on failure.
-* **$1** (string): A path to resolve.
-
-#### Exit codes
-
-* **0**: If successful.
-* **1**: If the path does not exists, an argument is missing or more
-
-#### Output on stdout
-
-* The resolved absolute path.
+@deprecated
 
 #### See also
 
--- src/realpath_check.bash
+++ src/realpath_check.bash
@@ -11,25 +11,6 @@ source "${BASH_SOURCE[0]%/*}/realpath-check.bash"
 # If the file does not exists, display an error message and return error.
 # Print its absolute real path on stdout if found.
 #
-# @example
-#     source "${BASH_SOURCE[0]%/*}/libs/biapy-bashlings/src/realpath_check.bash"
-#     file_path="../relative/path"
-#     if file_realpath="$( realpath_check "${file_path}" )"; then
-#       echo "File found. processing..."
-#     else
-#       exit 1
-#     fi
-#
-# @arg -q | --quiet Disable the error message.
-# @arg -e | --exit Enable exiting on failure.
-# @arg $1 string A path to resolve.
-#
-# @stdout The resolved absolute path.
-#
-# @exitcode 0 If successful.
-# @exitcode 1 If the path does not exists, an argument is missing or more
-#   than one argument given.
-#
 # @deprecated
 #
 # @see realpath-check
