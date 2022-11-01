cae61ea07ae32e43521e7155ed37b5ffb4fb3d81
Pierre-Yves Landuré <pierre-yves@landure.fr>
2022-11-01 18:34:32 +0100

    Allow doc to be created in the same subfolders as in src.

--- bs/doc.sh
+++ bs/doc.sh
@@ -4,8 +4,11 @@
 
 find ./src/ -name "*.bash" | \
   while read -r 'file'; do
-    filename="$( basename "${file}" | sed -e 's/\.[^\.]*sh$//' )"
+    filename="$( echo "${file}" \
+      | sed -e 's|^./src/||' \
+        -e 's/\.[^\.]*sh$//' )"
     doc="./doc/${filename}.md"
+    mkdir --parent "$( dirname "${doc}" )"
     echo "Building documentation for ${file} in ${doc}"
     ./libs/shdoc/shdoc < "${file}" > "${doc}"
   done
