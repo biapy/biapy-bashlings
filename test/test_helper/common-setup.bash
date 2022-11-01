ddaa5546dcb055208012486c8849ec9653bde223
Pierre-Yves Landuré <pierre-yves@landure.fr>
2022-11-01 18:33:58 +0100

    Allow for tests files in sub folders of test (eg test/internals).

--- test/test_helper/common-setup.bash
+++ test/test_helper/common-setup.bash
@@ -5,14 +5,15 @@
 # Set PROJECT_ROOT environment variable.
 # Add PROJECT_ROOT/src to PATH.
 _common_setup() {
-    load 'test_helper/bats-support/load'
-    load 'test_helper/bats-assert/load'
+    load "${BASH_SOURCE[0]%/*}/bats-support/load"
+    load "${BASH_SOURCE[0]%/*}/bats-assert/load"
     # get the containing directory of this file
     # use $BATS_TEST_FILENAME instead of ${BASH_SOURCE[0]} or $0,
     # as those will point to the bats executable's location or the preprocessed file respectively
-    PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 2>&1 && pwd )"
+    # shellcheck disable=SC2154
+    PROJECT_ROOT="$( cd "${BATS_TEST_FILENAME%\/test\/*}/" >/dev/null 2>&1 && pwd )"
     # make executables in src/ visible to PATH
-    PATH="$PROJECT_ROOT/src:$PATH"
+    PATH="${PROJECT_ROOT}/src:${PATH}"
 }
 
 # Common test environment teardown.
