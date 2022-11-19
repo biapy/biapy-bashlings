71cf80dd5242aba5f3bab0079552d05d10933397
Pierre-Yves Landuré <pierre-yves@landure.fr>
2022-11-19 17:21:19 +0100

    Add code to work on macos.

--- src/realpath.bash
+++ src/realpath.bash
@@ -25,6 +25,8 @@ source "${BASH_SOURCE[0]%/*}/cecho.bash"
 # @stdout The resolved absolute path, or empty string if realpath not found.
 # @stderr Error if argument is missing or more than one argument given.
 #
+# @see [How can I get the behavior of GNU's readlink -f on a Mac?](https://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac)
+# @see [bats-core bats command](https://github.com/bats-core/bats-core/blob/master/bin/bats)
 # @see [cecho](./cecho.md#cecho)
 function realpath() {
   # Accept one and only one argument.
@@ -33,39 +35,66 @@ function realpath() {
     return 1
   fi
 
+  # Fail on empty argument.
+  [[ -z "${1-}" ]] && return 1
+
   local realpath=""
 
-  case "$(uname)" in
-    'Linux')
-      realpath="$(
-        readlink -f -- "${1}" \
-          || /usr/bin/realpath -- "${1}" 2> '/dev/null'
-      )"
-      ;;
-    'Darwin')
-      # Use python to find a realpath similar to the linux one.
-      # See [Bash script absolute path with OS X](https://stackoverflow.com/questions/3572030/bash-script-absolute-path-with-os-x)
-      # If a pure bash solution exists, please make a pull request.
-      python="$(
-        type -p 'python3' 2> '/dev/null' \
-          || type -p 'python' 2> '/dev>/null'
-      )"
-      realpath="$(
-        stat -f '%N' -- "${1}" 2> '/dev/null' \
-          || [[ -n "${python}" ]] \
-          && "${python}" 2> '/dev/null' << EOF
-import os
-import sys
-print(os.path.realpath(u"${1}"))
-EOF
-      )"
-      ;;
-    *)
-      realpath="$(
-        /usr/bin/realpath -- "${1}" 2> '/dev/null'
-      )"
-      ;;
-  esac
+  if command -v 'greadlink' > '/dev/null'; then
+    realpath="$(greadlink -f -- "${1-}" 2> '/dev/null')"
+  elif ! realpath="$(readlink -f -- "${1-}" 2> '/dev/null')"; then
+    # If readlink -f is not available (e.g. MacOS), use bats-core inspired
+    # alternative.
+    # Follow up to 40 symbolic links
+    local max_symlinks=40
+    target="${1-}"
+    realpath=""
+
+    # If target exist, trim trailing slashes
+    [[ -e "${target%/}" ]] || target=${1%"${1##*[!/]}"}
+    # If target is a directory, add trailing slash.
+    [[ -d "${target:-/}" ]] && target="${target}/"
+
+    # Check that cd -P work, fail if not.
+    cd -P . 2> /dev/null || return 1
+    # While symbolic link limit is not broken.
+    while [[ "${max_symlinks}" -ge 0 ]] \
+            && max_symlinks=$((max_symlinks - 1)); do
+      # If target is a filename without path (?)
+      if [[ "${target}" != "${target%/*}" ]]; then
+        # Ensure path exists.
+        case "${target}" in
+          /*)
+            cd -P "${target%/*}/" 2> '/dev/null' \
+              || break
+            ;;
+          *)
+            cd -P "./${target%/*}" 2> '/dev/null' \
+              || break
+            ;;
+        esac
+        target="${target##*/}"
+      fi
+
+      # If target is not a symbolic link.
+      if [ ! -L "${target}" ]; then
+        # Output result.
+        target="${PWD%/}${target:+/}${target}"
+        # printf '%s\n' "${target:-/}"
+        # return 0
+        realpath="${target:-/}"
+        break
+      fi
+
+      # `ls -dl` format: "%s %u %s %s %u %s %s -> %s\n",
+      #   <file mode>, <number of links>, <owner name>, <group name>,
+      #   <size>, <date and time>, <pathname of link>, <contents of link>
+      # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/ls.html
+      link="$(ls -dl -- "{$target}" 2> /dev/null)" || break
+      target="${link#*" ${target} -> "}"
+    done
+    # return 1
+  fi
 
   # If realpath is not found, return with error.
   [[ -z "${realpath}" ]] && return 1
